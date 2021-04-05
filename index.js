81b1f54611f35a8e68b8e54e3335123ef24a866d
Marwa  Awwad <marwaawwad10@gmail.com>
2021-04-05 23:46:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 
 // Write your code here
+let multiply,num1=31,num2=2,num3=24,num4=5,max,mod,random;
+multiply=num1*num2;
+random=Math.floor(Math.random() * 10) + 1;
+mod=num3 % num4;
+max=Math.max(1,20,12,19);
