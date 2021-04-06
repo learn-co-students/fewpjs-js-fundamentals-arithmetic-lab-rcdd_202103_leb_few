4ab4f2c8f461e7eac32bc0024df88634dd2e82bc
Hazar Saad ElDine <saadeldinehazar@gmail.com>
2021-04-06 21:36:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+var num1=2;
+var num2=31;
+var multiply = num1*num2;
+
+var random=Math.random() * 10;
+
+var num3=19;
+var num4=4;
+var mod = num3%num4;
+
+var max=Math.max(1, 2, 3, 4, 20);
