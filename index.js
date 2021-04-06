e22b1472ab2dca3bd76de3b89a45cd436d1f7449
Amro dandashli <amrdandashli@gmail.com>
2021-04-06 18:43:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-var multiply = num1 * num2;
-var random  = 1 ;
-var mod = num3 % num4;
-var max += Math.max()
+
  // Write your code here
+
+ var num2= 31 ;
+ var num1= 2 ;
+ var num3 = 21;
+ var num4 = 17;
+  var multiply = num1 * num2;
+  var random  = Math.floor(6) ;
+  var mod = num3 % num4;
+  var max  = Math.max(1,2,3,4,5,20)
