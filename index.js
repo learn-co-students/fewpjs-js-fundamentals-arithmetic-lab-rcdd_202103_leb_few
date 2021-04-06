f79309c32f665a7387ba6efcd882ee5550937d93
Hazem El Sankari <hazemleb96@gmail.com>
2021-04-06 17:52:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+let num1=31
+let num2=2
+let multiply=num1*num2;
+
+let random = Math.ceil(Math.random());
+let num3=29;
+let num4=5;
+let mod = num3%num4
+
+let max = Math.max(20,18,19)
