0df48bd6dd1eb7241d864d1d9c20ea6d217c3d10
Ali Kabalan <ali.kabalan.1994@gmail.com>
2021-04-10 09:14:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+//No.1
+let num1=31;
+let num2=2;
+let multiply=num1*num2;
 
-// Write your code here
+//No.2
+const random = Math.floor(Math.random() * 10) ;
+
+//No.3
+let num3=104;
+let num4=20;
+let mod=num3%num4;
+
+
+
+//No.4
+const max = Math.max(2, 5, 9, 20, 17, 12);
