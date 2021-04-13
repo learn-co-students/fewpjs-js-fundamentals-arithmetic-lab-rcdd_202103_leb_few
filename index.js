95df7a37e92370b3528aa053aa56f23f62e61d4e
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-04-13 16:09:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
+
+const random = Math.floor(Math.random() + 1);
+
+let num3 = 19;
+let num4 = 5;
+const mod = num3 % num4;
+
+
+const max = Math.max(20);
