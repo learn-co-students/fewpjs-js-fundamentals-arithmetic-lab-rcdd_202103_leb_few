42e88ee9c493253cde300705412c1980ce4ab974
Mira Chami <miramc149@gmail.com>
2021-04-18 22:25:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 // Write your code here
-let multiply = 2 * 31;
-Math.random(1);
-let mod= num3%num4;
-let max(20,10);
+const num1 = 31
+const num2 = 2
+const num3 = 13
+const num4 = 9
+const set = (1,2,3,4,4,5,6,7,8,9,20)
+const multiply = num1*num2
+const random = Math.floor(Math.random()*8 +1)
+const mod = num3 % num4
+const max = Math.max(set)
