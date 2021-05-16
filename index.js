e1361e6780dc87894bedb9efc6e4e810f2bea22f
Adnan Sayed <adnan.sayed.official@gmail.com>
2021-05-16 14:01:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-var multiply = num1 * num2;
-function multiply(num1,num2){
-  return num1*num2;
-}
-var random = Math.random()+1;
-var mod = num3 % num4 ;
-var max = Math.max(1,2,3,20);
-var c =1;
+
+// Write your code here
+
+const num1 = 2
+const num2 = 31
+const multiply = num1*num2
+
+const random = Math.ceil( Math.random() * 100 )
+
+
+const num3 = 14
+const num4 = 5
+const mod = num3%num4
+const max = Math.max( 5, 10, 15, 20 )
