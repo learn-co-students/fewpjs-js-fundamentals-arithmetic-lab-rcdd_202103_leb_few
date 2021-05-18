01443d29d70215a522bce81417a903dfdf420e80
Nour baltaji <nourbaltaji99@gmail.com>
2021-05-18 18:59:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 9;
+let num4 = 5;
+let multiply = num1 * num2;//62
+let random = Math.floor(Math.random() * 10) + 1; // >0
+let mod =  num3 % num4; //4
+let max = Math.max(1, 2, 3, 4, 20);
